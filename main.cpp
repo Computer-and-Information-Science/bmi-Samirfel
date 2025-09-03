70fcb5f69e2ad7453c1ea2c2c88e089bca4f6a67
Samirfel <samirfel012@gmail.com>
2025-09-03 15:26:25 +0000

    Wrote code to calculate a person's bmi

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 int main() {
- 
+double meters; 
+double kilograms;
 
+cout <<"Converting imperial to metric"<< endl;
 
+int height = 61;
+int weight = 130;
+meters = height * 0.0254;
+kilograms = weight * 0.453592;
+double bmi = kilograms / (meters * meters);
+
+cout <<"The BMI for this person is :" << bmi <<"."<< endl;
 }
